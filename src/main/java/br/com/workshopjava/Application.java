80b5838816f5298c7b159f448859dac1a1b75901
Thales Assis <thaleslima-19@hotmail.com>
2019-07-17 12:32:58 -0300

    Creating instantiation operation

--- src/main/java/br/com/workshopjava/Application.java
+++ src/main/java/br/com/workshopjava/Application.java
@@ -1,13 +1,34 @@
 package br.com.workshopjava;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.workshopjava.domain.Categoria;
+import br.com.workshopjava.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
