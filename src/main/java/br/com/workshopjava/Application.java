3638cc0d28783e9df2dca1664e83945702a44c0e
Thales Assis <thaleslima-19@hotmail.com>
2019-07-17 12:40:47 -0300

    Produto and association many to many

--- src/main/java/br/com/workshopjava/Application.java
+++ src/main/java/br/com/workshopjava/Application.java
@@ -1,5 +1,6 @@
 package br.com.workshopjava;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.workshopjava.domain.Categoria;
+import br.com.workshopjava.domain.Produto;
 import br.com.workshopjava.repository.CategoriaRepository;
+import br.com.workshopjava.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -16,7 +19,9 @@ public class Application implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
-
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -29,6 +34,20 @@ public class Application implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
+		Produto p1 = new Produto(null, "Computador", new BigDecimal("2000.00"));
+		Produto p2 = new Produto(null, "Impressora", new BigDecimal("800.00"));
+		Produto p3 = new Produto(null, "Mouse", new BigDecimal("80.00"));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
