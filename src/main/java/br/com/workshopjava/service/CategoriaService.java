b6c5ed1a5bba530f166ec27c15ed2f78736d2343
Thales Assis <thaleslima-19@hotmail.com>
2019-07-17 16:04:46 -0300

    Deleting a Categoria with DELETE

--- src/main/java/br/com/workshopjava/service/CategoriaService.java
+++ src/main/java/br/com/workshopjava/service/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.workshopjava.domain.Categoria;
 import br.com.workshopjava.repository.CategoriaRepository;
+import br.com.workshopjava.service.exception.DataIntegrityException;
 import br.com.workshopjava.service.exception.ObjectNotFoundException;
 
 @Service
@@ -15,24 +17,32 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> categoria = repository.findById(id);
-		return categoria.orElseThrow( () -> new ObjectNotFoundException("Object not found! Id: " + id));
+		return categoria.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: " + id));
 	}
-	
+
 	public Categoria insert(Categoria categoria) {
 		categoria.setId(null);
 		return repository.save(categoria);
 	}
-	
+
 	public Categoria update(Categoria categoria) {
 		findById(categoria.getId());
 		return repository.save(categoria);
 	}
 
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("It is not possible to delete a Category that has Products!");
+		}
+	}
 }
